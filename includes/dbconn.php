e6c5ba0946c289c529e8d7b02109a0b9ece5faf6
Anmolpoojari <152378436+Anmolpoojari@users.noreply.github.com>
2024-03-23 20:19:59 +0530

    Add files via upload

---
+++ includes/dbconn.php
@@ -0,0 +1,7 @@
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost:3308";////////////////////////////
+    $db="hostelmsphp";
+    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
